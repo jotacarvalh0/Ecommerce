f3c57c493a74b0010310ba70cb9febdf0e3114e7
jotacarvalh0 <josecarvalho11235@gmail.com>
2025-08-30 17:21:01 -0300

    FEAT:(checkout) Implement cart UI and add shipping address schema.

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -15,6 +15,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "../ui/sheet";
+import { Cart } from "./cart";
 
 export const Header = () => {
   const { data: session } = authClient.useSession();
@@ -24,7 +25,8 @@ export const Header = () => {
         <Image src="/logo.svg" alt="BEWEAR" width={100} height={26.14} />
       </Link>
 
-      <div className="itens-center flex">
+      <div className="itens-center flex gap-3">
+        <Cart />
         <Sheet>
           <SheetTrigger asChild>
             <Button variant="outline" size="icon">
