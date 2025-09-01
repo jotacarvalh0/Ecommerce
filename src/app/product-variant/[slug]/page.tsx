791ff8a2702b1bc8d76bb6bf85c61d5c7ae69195
jotacarvalh0 <josecarvalho11235@gmail.com>
2025-08-31 23:35:44 -0300

    FEAT:(cart) Implement react-query for state management.

--- src/app/product-variant/[slug]/page.tsx
+++ src/app/product-variant/[slug]/page.tsx
@@ -5,12 +5,11 @@ import { notFound } from "next/navigation";
 import Footer from "@/components/common/footer";
 import { Header } from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
-import { Button } from "@/components/ui/button";
 import { db } from "@/db";
 import { productTable, productVariantTable } from "@/db/schema";
 import { formatCentsToBRL } from "@/helpers/money";
 
-import QuantitySelector from "./components/quantity-selector";
+import ProductActions from "./components/product-actions";
 import VariantSelector from "./components/variant-selector";
 
 interface ProductVariantPageProps {
@@ -71,26 +70,7 @@ const ProductVariantPage = async ({ params }: ProductVariantPageProps) => {
           </h3>
         </div>
 
-        <div className="px-5">
-          {/* Quantidade */}
-          <QuantitySelector />
-        </div>
-
-        <div className="flex flex-col space-y-4 px-5">
-          {/* Botões */}
-          <Button className="rounded-full" size="lg" variant="outline">
-            Adicionar ao carrinho
-          </Button>
-          <Button className="rounded-full" size="lg">
-            Comprar agora
-          </Button>
-        </div>
-
-        <div className="px-5">
-          <p className="text-shadow-amber-600">
-            {productVariant.product.description}
-          </p>
-        </div>
+        <ProductActions productVariantId={productVariant.id} />
 
         <ProductList title="Talvez você goste" products={likelyProducts} />
 
