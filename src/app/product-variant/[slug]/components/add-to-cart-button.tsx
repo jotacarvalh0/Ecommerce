027bb91b96fd4345fd0762c8c59f064946018527
jotacarvalh0 <josecarvalho11235@gmail.com>
2026-03-09 12:17:58 -0300

    FEAT:(cart) Ensure product can be removed from cart

--- src/app/product-variant/[slug]/components/add-to-cart-button.tsx
+++ src/app/product-variant/[slug]/components/add-to-cart-button.tsx
@@ -36,7 +36,7 @@ const AddToCartButton = ({
       onClick={() => mutate()}
     >
       {isPending && <Loader2 className="animate-spin" />}
-      Adicionar à sacola
+      Adicionar ao carrinho
     </Button>
   );
 };
