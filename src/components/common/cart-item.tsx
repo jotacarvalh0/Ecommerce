9c31036fe769e162397ab6899c656028e2ff7938
jotacarvalh0 <josecarvalho11235@gmail.com>
2026-03-09 20:03:28 -0300

    FEAT:(car) Ensure product quantity can be decreased.

--- src/components/common/cart-item.tsx
+++ src/components/common/cart-item.tsx
@@ -4,9 +4,10 @@ import Image from "next/image";
 import { formatCentsToBRL } from "@/helpers/money";
 
 import { Button } from "../ui/button";
-import { QueryClient, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { removeProductFromCart } from "@/actions/remove-cart-product";
 import { toast } from "sonner";
+import { decreaseCartProductQuantity } from "@/actions/decrease-cart-product-quantity";
 
 interface CartItemProps {
   id: string;
@@ -34,6 +35,14 @@ const CartItem = ({
     }
   });
 
+  const decreaseCartProductQuantityMutation = useMutation({
+    mutationKey: ['decrease-cart-product-quantity'],
+    mutationFn: () => decreaseCartProductQuantity({ cartItemId: id }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["cart"] });
+    }
+  });
+
   const handleDeleteClick = () => {
     removeProductFromCartMutation.mutate(undefined, {
       onSuccess: () => {
@@ -41,6 +50,17 @@ const CartItem = ({
       },
       onError: () => {
         toast.error("Erro ao remover produto do carrinho.")
+      }
+    })
+  };
+
+  const handleDecreaseQuantityClick = () => {
+    decreaseCartProductQuantityMutation.mutate(undefined, {
+      onSuccess: () => {
+        toast.success("Quantidade do produto diminuida.")
+      },
+      onError: () => {
+        toast.error("Erro ao diminuir quantidade do produto.")
       }
     })
   };
@@ -60,7 +80,7 @@ const CartItem = ({
             {productVariantName}
           </p>
           <div className="flex w-[100px] items-center justify-between rounded-lg border p-1">
-            <Button className="h-4 w-4" variant="ghost" onClick={() => {}}>
+            <Button className="h-4 w-4" variant="ghost" onClick={handleDecreaseQuantityClick}>
               <MinusIcon />
             </Button>
             <p className="text-xs font-medium">{quantity}</p>
