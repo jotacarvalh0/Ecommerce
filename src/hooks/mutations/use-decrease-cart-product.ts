a959e1ae9cb975b5fdc1899b594134117c4ee98d
jotacarvalh0 <josecarvalho11235@gmail.com>
2026-03-09 23:20:00 -0300

    REFACTOR:(cart) Add hooks to queries and mutations.

---
+++ src/hooks/mutations/use-decrease-cart-product.ts
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { decreaseCartProductQuantity } from "@/actions/decrease-cart-product-quantity";
+
+import { getUseCartQueryKey } from "../queries/use-cart";
+
+export const getUseDecreaseCartProductMutationKey = (cartItemId: string) =>
+  ["decrease-cart-product-quantity", cartItemId] as const;
+
+export const useDecreaseCartProduct = (cartItemId: string) => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationKey: getUseDecreaseCartProductMutationKey(cartItemId),
+    mutationFn: () => decreaseCartProductQuantity({ cartItemId }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: getUseCartQueryKey() });
+    },
+  });
+};
